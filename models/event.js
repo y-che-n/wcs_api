59f6b1341a824bb2ad207c621ed292a18789151a
y-che-n <ychen374@illinois.edu>
2018-11-13 14:01:04 -0600

    242 week 1

--- models/event.js
+++ models/event.js
@@ -1,8 +1,18 @@
 var mongoose = require('mongoose');
 
+/*
+  Schema for Event db objects
+  name: event name
+  points: number of points per event (GM/Code Ada/ChicTech - 2, all other events - 1)
+  dateCreated: date event object was put into
+  date: actual event date
+  attendees: array of netids of everyone who signed in
+*/
+
 var EventSchema = new mongoose.Schema({
     name: String,
     points: Number,
+    category: String,
     dateCreated: { type: Date, default: Date.now },
     date: { type: Date, default: Date.now },
     attendees: { type: Array, default: []}},
