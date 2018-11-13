211582351b7c81e56c23c42a0a40668be9786bb0
y-che-n <ychen374@illinois.edu>
2018-11-13 14:43:25 -0600

    242 week 1

--- models/user.js
+++ models/user.js
@@ -1,7 +1,16 @@
 var mongoose = require('mongoose');
 
+/*
+  Schema for User db objects
+  netid: Illinois netid
+  points: event points for user (will be updated to also include office hour + committee points)
+  office_hours: array of all office hour dates attended
+  committees: array of all committee meeting dates attended
+*/
+
 var UserSchema = new mongoose.Schema({
     netid: String,
+    points: Number,
     office_hours: { type: Array, default: []},
     committees: { type: Array, default: []}},
     { usePushEach: true});
